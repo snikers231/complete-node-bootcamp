e5c9c5d0451f9fa82284d50a4d53b835b4f0367a
Alexandr Karnyuk <karnyuk.alex@gmail.com>
2020-11-01 22:32:37 +0300

    ended project

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -20,18 +20,71 @@ const url = require("url");
 // fs.readFile("./txt/read-this.txt", "utf-8", (error, data) => {
 //   console.log(data);
 // })
+
+const replaceTemplate = (template, data) => {
+  return template.replace(/{%PRODUCTNAME%}/g, data.productName)
+    .replace(/{%IMAGE%}/g, data.image)
+    .replace(/{%QUANTITY%}/g, data.quantity)
+    .replace(/{%PRICE%}/g, data.price)
+    .replace(/{%ID%}/g, data.id)
+    .replace(/{%NUTRIENTS%}/g, data.nutrients)
+    .replace(/{%DESCRIPTION%}/g, data.description)
+    .replace(/{%FROM%}/g, data.from)
+    .replace(/{%NOT_ORGANIC%}/g, data.organic ? "not-organic" : "")
+}
+
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, "utf-8");
+const parsedData = JSON.parse(data);
+
+const templateCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, "utf-8");
+const templateOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, "utf-8");
+const templateProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, "utf-8");
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/") {
-    res.end("This is a root");
-  } else if (pathName === "/overview") {
-    res.end("Thit is an OVERVIEW");
+  const urlObj = url.parse(req.url, true);
+  const pathName = urlObj.pathname;
+
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = parsedData.map(
+        cardData => replaceTemplate(templateCard, cardData)
+      ).join("");
+    const output = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.end(output);
+
+  // Product page
   } else if (pathName === "/product") {
-    res.end("Thit is an PRODUCT PAGE ");
+    const product = parsedData.find((obj) => obj.id === +urlObj.query.id);
+
+    if (product) {
+      const output = replaceTemplate(templateProduct, product);
+      res.writeHead(200, {
+        "Content-type": "text/html",
+      });
+      res.end(output);
+    } else {
+      res.writeHead(404, {
+        "Content-type": "text/html",
+      });
+      res.end("<h1>Product not found</h1>");
+    }
+
+  // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+  // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
-      "MyOwnHeader": "TestHeader",
     });
     res.end("<h1>404 Page not found</h1>");
   }
